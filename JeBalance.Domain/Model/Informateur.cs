8dd5eed8f1776b976083134515c86b07b0d5be97
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-01-20 19:01:17 +0100

    formatted code

--- JeBalance.Domain/Model/Informateur.cs
+++ JeBalance.Domain/Model/Informateur.cs
@@ -30,7 +30,8 @@ public class Informateur : Personne
     {
         Nom = nom;
         Prenom = prenom;
-        Adresse = adresse; }
+        Adresse = adresse;
+    }
 
     public Informateur(int id, Nom nom, Nom prenom, Adresse adresse, bool estCalomniateur) : base(id)
     {
