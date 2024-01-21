c4da415491cb0a0d0a21e1e55faf0cf1983a19c6
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-01-21 13:48:37 +0100

    add: entity framework design nuget package + initial auth db migrations

--- JeBalance.API.Interne.Securisee/Authentication/AuthDbContext.cs
+++ JeBalance.API.Interne.Securisee/Authentication/AuthDbContext.cs
@@ -5,20 +5,10 @@ namespace JeBalance.API.Interne.Securisee.Authentication;
 
 public class AuthDbContext : IdentityDbContext<ApplicationUser>
 {
-    public AuthDbContext()
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
     {
     }
-
-    public AuthDbContext(DbContextOptions options) : base(options)
-    {
-    }
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        builder.HasDefaultSchema("auth");
-        base.OnModelCreating(builder);
-    }
-
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured) optionsBuilder.UseSqlite("Data Source=LocalDatabase.db");
