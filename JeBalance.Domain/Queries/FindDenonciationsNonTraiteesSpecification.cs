92d08b589a1cb3129365c7967346044e334b7cec
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-01-21 14:43:54 +0100

    add: find denonciations non traitées query + handler + specification

---
+++ JeBalance.Domain/Queries/FindDenonciationsNonTraiteesSpecification.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+using JeBalance.Domain.Contracts;
+using JeBalance.Domain.Model;
+
+namespace JeBalance.Domain.Queries;
+
+public class FindDenonciationsNonTraiteesSpecification: Specification<Denonciation>
+{
+    
+    public override Expression<Func<Denonciation, bool>> ToExpression()
+    {
+        return denonciation => denonciation.ReponseId == null;
+    }
+}
