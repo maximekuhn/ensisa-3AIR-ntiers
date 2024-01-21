1a5bfd0065054136d19357120b193c69ad343a53
albig <albi.grainca@uha.fr>
2024-01-21 16:54:47 +0100

    fix bug

--- JeBalance.API.Secrete.Securisee/Resources/VIPAPI.cs
+++ JeBalance.API.Secrete.Securisee/Resources/VIPAPI.cs
@@ -1,17 +1,9 @@
 using System.Text.Json.Serialization;
-using JeBalance.Domain.Model;
 
 namespace JeBalance.API.Secrete.Securisee.Resources;
 
 public class VIPAPI
 {
-    public VIPAPI(VIP source)
-    {
-        Nom = source.Nom;
-        Prenom = source.Prenom;
-        Adresse = new AdresseAPI(source.Adresse);
-    }
-
     [JsonPropertyName("nom")] public string Nom { get; set; }
 
     [JsonPropertyName("prenom")] public string Prenom { get; set; }
