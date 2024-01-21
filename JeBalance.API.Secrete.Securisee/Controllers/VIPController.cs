943d37c5de23edfe219882c9fdb98ffa669da49c
albig <albi.grainca@uha.fr>
2024-01-21 16:25:31 +0100

    Add : create VIP with api

---
+++ JeBalance.API.Secrete.Securisee/Controllers/VIPController.cs
@@ -0,0 +1,29 @@
+using JeBalance.API.Secrete.Securisee.Resources;
+using JeBalance.Domain.Commands;
+using JeBalance.Domain.Model;
+using JeBalance.Domain.ValueObjects;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JeBalance.API.Secrete.Securisee.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class VIPController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public VIPController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateVIP([FromBody] VIPAPI resource)
+    {
+        var createVIPCommand = new CreateVIPCommand(resource.Nom, resource.Prenom, new Adresse(new NumeroVoie(resource.Adresse.NumeroVoie), new NomVoie(resource.Adresse.NomVoie), new CodePostal(resource.Adresse.CodePostal), new NomCommune(resource.Adresse.NomCommune)));
+        var vipId = await _mediator.Send(createVIPCommand);
+        return Ok(vipId);
+    }
+    
+}
