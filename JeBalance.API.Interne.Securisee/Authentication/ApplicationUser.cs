fb20bf355d2b5030b768629f02667d86a772f8f6
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-01-21 11:45:50 +0100

    add: authentication base

---
+++ JeBalance.API.Interne.Securisee/Authentication/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace JeBalance.API.Interne.Securisee.Authentication;
+
+public class ApplicationUser: IdentityUser
+{
+    public int AssociatedAdministrateurFiscaleId { get; set; }
+}
---
+++ JeBalance.API.Interne.Securisee/Authentication/AuthDbContext.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JeBalance.API.Interne.Securisee.Authentication;
+
+public class AuthDbContext: IdentityDbContext<ApplicationUser>
+{
+    public AuthDbContext()
+    {
+    }
+
+    public AuthDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("auth");
+        base.OnModelCreating(builder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=LocalDatabase.db");
+        }
+    }
+}
