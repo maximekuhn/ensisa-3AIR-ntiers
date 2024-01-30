9c1f4662edc8898d3b29ca3e80802d04cd373242
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2024-01-30 21:40:15 +0100

    CI: NomCOmmune

--- JeBalance.Domain/ValueObjects/NomCommune.cs
+++ JeBalance.Domain/ValueObjects/NomCommune.cs
@@ -4,13 +4,22 @@ namespace JeBalance.Domain.ValueObjects;
 
 public class NomCommune : SimpleValueObject<string>
 {
+    public const int MIN_LENGTH = 3;
+    public const int MAX_LENGTH = 99;
     public NomCommune(string value) : base(value)
     {
     }
 
     public override string Validate(string value)
     {
-        // TODO: real validation
+        var trimmedValue = value.Trim();
+        if (trimmedValue.Length < MIN_LENGTH)
+            throw new ApplicationException($"Le nom de commune doit faire au minimum {MIN_LENGTH} caractère(s)");
+
+        if (trimmedValue.Length > MAX_LENGTH)
+            throw new ApplicationException(
+                $"Le nom de commune ne peut pas excéder une longueur de plus de {MAX_LENGTH} caractères");
+        
         return value;
     }
 }
